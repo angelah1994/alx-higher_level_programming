8bd2348e4c86acf9d093a2b4bfb0da47c2db3121
angelah1994 <wafulaangy@gmail.com>
2022-06-01 14:59:57 -0700

    hi

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,4 @@
-#!usr/bin/python3
-for ascii_num in range(122, 96, -1):
-    if ascii_num % 2 == 1:
-        ascii_num = ascii_num - 32
+#!/usr/bin/python
+for ascii_num % 2 == 1:
+    ascii_num = ascii_num - 32
     print("{:c}".format(ascii_num), end='')
