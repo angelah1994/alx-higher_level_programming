2349cffd2e4cd0f84da291701d49c3d0b68b96c0
angelah1994 <wafulaangy@gmail.com>
2022-06-05 07:19:33 -0700

    hello

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
+
+def sub(a, b):
+    """My substarction function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a - b
+    """
+    return (a - b)
+
+def mul(a, b):
+    """My mu;tiplication function
+
+    Args:
+        a: first integer
+        b: second integer
+    Returns:
+        The return value. a * b
+    """
+    return (a * b)
+
+def div(a, b):
+    """My division function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a / b
+    """
+    return int(a / b)
