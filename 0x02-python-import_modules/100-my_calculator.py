c4ab7ada0edac277c6e59f3305c8be16e94c6da9
angelah1994 <wafulaangy@gmail.com>
2022-06-07 19:56:02 -0700

    add

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,47 +2,23 @@
 from sys import argv
 from calculator_1 import add, sub, mul, div
 
+ops = ['+', '-', '*', '/']
+argc = len(argv)
+
 if __name__ == '__main__':
-    """
-    Handles basic operations
-
-    Performs basic operations like addition, substraction,
-    multiplication and division between two numbers.
-
-    The program will execute an operation between two numbers
-    selected by the operator sent to the program.
-    """
-    l_av = len(argv) - 1
-
-    if l_av == 3:
-        operator = argv[2]
-        num_a = int(argv[1])
-        num_b = int(argv[3])
-        if operator == '+':
-            res = add(num_a, num_b)
-            print('{:d} + {:d} = {:d}'.format(num_a, num_b, res))
-            exit(0)
-        elif operator == '-':
-            res = sub(num_a, num_b)
-            print('{:d} - {:d} = {:d}'.format(num_a, num_b, res))
-            exit(0)
-        elif operator == '*':
-            res = mul(num_a, num_b)
-            print('{:d} * {:d} = {:d}'.format(num_a, num_b, res))
-            exit(0)
-        elif operstor == '/':
-            res = div(num_a, num_b)
-            print('{:d} / {:d} = {:d}'.format(num_a, num_b, res))
-            exit(0)
+    if (argc - 1) < 3:
+        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+        exit(1)
+    elif argv[2] not in ops:
+        print('Unknown operator. Available operators: +, -, * and /')
+        exit(1)
+    else:
+        a, b = int(argv[1]), int(argv[3])
+        if argv[2] == '+':
+            print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
+        elif argv[2] == '-':
+             print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
+        elif argv[2] == '*':
+            print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
         else:
-            print('Unknown operator. Available operators: +, -, * and /')
-            exit(1)
-else:
-     print('Usage: ./100-my_calculator.py <a> <operator> <b>')
-     exit(1)
-
-
-
-
-
-
+            print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))
